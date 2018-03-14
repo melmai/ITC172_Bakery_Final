73555483c1ad13c64473451775a4442bd08413a7
Mel Wong <mellymai@gmail.com>
2018-03-13 17:57:09 -0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            BakeryEntities db = new BakeryEntities();
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Baking since 2018";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Please contact us for special orders and catering events.";
+
+            return View();
+        }
+    }
+}
