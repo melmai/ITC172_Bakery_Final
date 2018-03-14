0b2cd79426cb387012682a4663d54c35963b1a38
Mel Wong <mellymai@gmail.com>
2018-03-13 20:51:10 -0700

    Adds class for new order and beginning of NewOrder Controller

---
+++ Controllers/NewOrderController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+    public class NewOrderController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+
+        // GET: NewOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index([Bind(Include = "PersonLastName, PersonFirstName, PersonEmail, PersonPhone")] NewOrder order)
+        {
+            Person person = new Person();
+            person.PersonLastName = order.PersonLastName;
+            person.PersonFirstName = order.PersonFirstName;
+            person.PersonEmail = order.PersonEmail;
+            person.PersonPhone = order.PersonPhone;
+            person.PersonDateAdded = DateTime.Now;
+            db.People.Add(person);
+
+            Sale sale = new Sale();
+            sale.SaleDate = DateTime.Now;
+
+            db.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ Models/NewOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bakery.Models
+{
+    public class NewOrder
+    {
+        /* Person Registration */
+        public string PersonLastName { get; set; }
+        public string PersonFirstName { get; set; }
+        public string PersonEmail { get; set; }
+        public string PersonPhone { get; set; }
+
+        /* Sale Details */
+        public int SaleDetailQuantity { get; set; }
+        public int ProductKey { get; set; }
+        public decimal SaleDetailPriceCharged { get; set; }
+        public decimal SaleDetailSaleTaxPercent { get; set; }
+    }
+}
